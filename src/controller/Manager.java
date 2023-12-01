6aca4a03779be149dfbe22ffd3506a7a69690897
Akheel Alameddin <aalam266@mtroyal.ca>
2023-12-01 04:32:17 -0700

    Completed base GUI's in view package

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -1,5 +1,39 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Manager {
-	
+
+    @FXML
+    private TextField serialNumberField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField typeField;
+
+    @FXML
+    private ListView<String> listView;
+
+    @FXML
+    private void handleSearch() {
+        // search logic here
+
+    }
+
+    @FXML
+    private void handleClear() {
+        serialNumberField.clear();
+        nameField.clear();
+        typeField.clear();
+        listView.getItems().clear();
+    }
+
+    @FXML
+    private void handleBuy() {
+        // buy logic here
+    }
 }
