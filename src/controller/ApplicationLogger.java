e56fa548182f2f6a28d7f684f60e9d0d8d58e6b7
Akheel Alameddin <aalam266@mtroyal.ca>
2023-12-02 18:08:29 -0700

    Added logger class

---
+++ src/controller/ApplicationLogger.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
+public class ApplicationLogger {
+	
+    private static Logger logger = Logger.getLogger("applog");
+    private static FileHandler fh;
+    
+    static {
+        try {
+            fh = new FileHandler("res/applog.log", true);
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+        } catch (SecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logWarning(String message) {
+        logger.warning(message);
+    }
+
+    public static void logSevere(String message) {
+        logger.severe(message);
+    }
+
+}
